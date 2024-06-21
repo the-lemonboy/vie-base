4bc0d7968a2cdaa0b32b61459e3468be934146b7
the-lemonboy <lingzhengpeng@outlook.com>
2024-06-21 10:09:23 +0800

    feat: 新增导入模版

--- src/main.js
+++ src/main.js
@@ -8,11 +8,14 @@ import router from './router'
 {{#if_eq import "full"}}
 import {{alias}} from '{{UI}}'
 import '{{css}}'
-{{/if_eq}}
 
 // import ElementPlus from 'element-plus';
 // import 'element-plus/dist/index.css'
 const app = createApp(App)
+{{/if_eq}}
+{{#if_eq import "full"}}
+app.use({{alias}})
+{{/if_eq}}
 
 app.use(createPinia())
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
+{{#if_eq import "demand"}}
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+{{/if_eq}}
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +10,25 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    {{#if_eq import "demand"}}
+    {{#if_eq UI "element-plus"}}
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    {{/if_eq}}
+    {{#if_eq UI "ant-design-vue"}}
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+    {{/if_eq}}
+    {{/if_eq}}
   ],
   resolve: {
     alias: {
